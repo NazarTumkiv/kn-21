ff5ac659fcd8c00bf4c1bdf453a985084d0b5a99
NazarTumkiv <nazart677@gmail.com>
2024-11-05 14:35:30 +0300

    lab2

---
+++ lab2/skrip.py
@@ -0,0 +1,9 @@
+while True:
+    msg = input("Please input msg: ")
+    if msg == "Добрий день":
+        print("Добрий день")
+    elif msg == "Слава Україні":
+        print("Героям слава")
+    elif msg == "па па":
+        print("До побачення!")
+        break
